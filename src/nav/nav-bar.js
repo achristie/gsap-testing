b18a209435f22add9366f47cbb35f7e775aa2d5f
Andrew Christie <awchristie86@gmail.com>
2016-09-05 22:06:40 -0400

    started on nav and search

---
+++ src/nav/nav-bar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './nav-bar.css';
+
+const Navbar = () => {
+  return (
+    <div className='nav-bar'>
+      <h2> IPREO </h2>
+      <h5> Digital Style Framework </h5>
+      <h6> Version 1.0.0 </h6>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/search/search-container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './search.css';
+
+const SearchContainer = () => {
+  return (
+    <div className='container'>
+      <h4> hello </h4>
+    </div>
+  );
+};
+
+export default SearchContainer;
